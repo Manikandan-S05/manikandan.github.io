4f900625e0ff2f66b45b6a274975c18fe55ba51a
manikandan <manikandan@ug.cusat.ac.in>
2022-07-24 23:54:10 +0530

    profile changed,contents updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="/assets/docs/Muthumanikandan's Resume.pdf" target="_blank">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
